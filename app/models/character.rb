efa445028a90dd2cb619acdfdc1a0a4bb7515c7e
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-01-21 20:49:04 -0500

    commit tree 1a86ac727cb322dfdc385a71c311c8730dbb4db7

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actors
+    belongs_to :shows
+    def say_that_thing_you_say
+        p "#{name} always says: #{self.catchphrase}"
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    def actors_list
+        characters.map do |character| #iterate through all characters for this show
+            "#{character.actors.full_name}" #map character's actor's full_name method 
+        end
+    end
 end
